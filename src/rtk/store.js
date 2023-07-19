e069e777adce68e55c8337d096b6dcba369f0852
se-bayan-elian <bezo2024@gmail.com>
2023-07-19 15:29:08 +0300

    fix theme store bug

--- src/rtk/store.js
+++ src/rtk/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit'
 import categoriesSlice from './slices/categoriesSlice';
 import productsSlice from './slices/productsSlice';
 import CartSlice from './slices/CartSlice';
+import Themes from './slices/Themes';
 const store = configureStore({ reducer: {
  categories :  categoriesSlice,
  products : productsSlice,
- cart : CartSlice
+ cart : CartSlice,
+ themes : Themes
 } })
 
 export default store
